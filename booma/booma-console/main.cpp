31704522cd0ee924e3325ca134458e538b88f869
Henrik Witt-Hansen <henrik@witt-hansen.dk>
2019-10-07 21:26:07 +0200

    Initial working tree

---
+++ booma/booma-console/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include "booma.h"
+
+int main(int argc, char** argv) {
+	BoomaInit(true);
+
+	return 0;
+}
--- booma/libbooma/booma.cpp
+++ booma/libbooma/booma.cpp
@@ -1,2 +1,15 @@
-/* Empty source file */
+#include <stdlib.h>
+#include <iostream>
 
+#include <hardtapi.h>
+
+void BoomaInit(bool verbose) {
+
+    // Initialize the Hardt toolkit.
+    // Set the last argument to 'true' to enable verbose output instead of logging to a local file
+    HInit(std::string("Booma"), verbose);
+
+    // Show library name and and Hardt version.
+    std::cout << "booma: using Hardt " + getversion() << std::endl;
+
+}
