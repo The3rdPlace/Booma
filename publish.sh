a8d843f04174c96cbf92fc9f27705866be46a463
Henrik Witt-Hansen <henrik@witt-hansen.dk>
2019-11-23 13:35:59 +0100

    Add publish rule to allow partial publishing from other os'es than linux

--- publish.sh
+++ publish.sh
@@ -16,13 +16,19 @@ MAJOR=$(cat booma/libbooma/include/booma.h | grep MAJORVERSION | cut -d ' ' -f 3
 MINOR=$(cat booma/libbooma/include/booma.h | grep MINORVERSION | cut -d ' ' -f 3)
 BUILD=$(cat booma/libbooma/include/booma.h | grep BUILDNO | cut -d ' ' -f 3)
 
-sed -i "s/{MAJORVERSION}/${MAJOR}/g" ../dist/site/index.html
-sed -i "s/{MINORVERSION}/${MINOR}/g" ../dist/site/index.html
-sed -i "s/{BUILD}/${BUILD}/g" ../dist/site/index.html
+sed -i -e "s/{MAJORVERSION}/${MAJOR}/g" ../dist/site/index.html
+sed -i -e "s/{MINORVERSION}/${MINOR}/g" ../dist/site/index.html
+sed -i -e "s/{BUILD}/${BUILD}/g" ../dist/site/index.html
+if [ ! -f "../dist/booma_${MAJOR}.${MINOR}-${BUILD}_amd64.deb" ]; then
+  sed -i -e "s/DEBVISIBLE/DEBHIDDEN/g" ../dist/site/index.html
+fi
 
 # Archives
-ncftpput -u $PUBUSER -p $PUBPASS $PUBHOST $PUBROOT/. ../dist/booma_${MAJOR}.${MINOR}-${BUILD}_amd64.deb
+if [ -f "../dist/booma_${MAJOR}.${MINOR}-${BUILD}_amd64.deb" ]; then
+  ncftpput -u $PUBUSER -p $PUBPASS $PUBHOST $PUBROOT/. ../dist/booma_${MAJOR}.${MINOR}-${BUILD}_amd64.deb
+fi
 ncftpput -u $PUBUSER -p $PUBPASS $PUBHOST $PUBROOT/. ../dist/booma_${MAJOR}.${MINOR}.orig.tar.gz
+
 
 # Page(s)
 ncftpput -u $PUBUSER -p  $PUBPASS -R $PUBHOST $PUBROOT/. ../dist/site/*
