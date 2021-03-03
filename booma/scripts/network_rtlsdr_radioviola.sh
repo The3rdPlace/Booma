40d9229231741e2a692a1f08e37241f8176a6b8c
Henrik Witt-Hansen <henrik@hardttoolkit.org>
2021-03-04 00:33:04 +0100

    Adjust scripts
    Please do note that these scripts is helping tools on my local
    machine.. You may use them as a shortcut to running booma, but
    most likely you will need to edit them

--- booma/scripts/network_rtlsdr_radioviola.sh
+++ booma/scripts/network_rtlsdr_radioviola.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make -j 10 && booma/booma-console/booma-console -o 0 -m AM -i NETWORK vlf 1722 1723 -dr 960000 -or 48000 -it Q -f 1440000 -rtlc 20 -rtlo 10000 $@
+booma/booma-console/booma-console -o 0 -m AM -i NETWORK vlf2 1722 1723 -dr 1152000 -or 48000 -it Q -is RTLSDR -f 1440000 -rtlc 20 -rtlo 5000 -o 1 $@
