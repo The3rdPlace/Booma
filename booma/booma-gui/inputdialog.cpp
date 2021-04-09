eeecad544d958a1bb0b208a0e3dd827a8c7407cc
Henrik Witt-Hansen <henrik@hardttoolkit.org>
2021-04-09 15:33:26 +0200

    When adding a new device, start with a minimal default

--- booma/booma-gui/inputdialog.cpp
+++ booma/booma-gui/inputdialog.cpp
@@ -199,8 +199,10 @@ void InputDialog::LoadState() {
 
     // Name of this input configuration
     if( _mode == EDIT ) {
+        // Use name of current configuration
         _name->value(_app->GetConfigSection().c_str());
     } else {
+        // Use a default name
         std::string newName;
         std::vector<std::string> sections = _app->GetConfigSections();
         for( int cnt = 0; ; cnt++ ) {
@@ -220,114 +222,144 @@ void InputDialog::LoadState() {
     }
 
     // Local or remote input ?
-    switch(_app->GetInputSourceType()) {
-        case NETWORK:
-            _isRemote->setonly();
-            break;
-        case AUDIO_DEVICE:
-            _isAudioDevice->setonly();
-            break;
-        case InputSourceType::RTLSDR:
-            _isRtlsdrDevice->setonly();
-            break;
-        case InputSourceType::PCM_FILE:
-            _isPcmFile->setonly();
-            break;
-        case InputSourceType::WAV_FILE:
-            _isWavFile->setonly();
-            break;
-        case InputSourceType::SILENCE:
-            _isSilence->setonly();
-            break;
-        case InputSourceType::SIGNAL_GENERATOR:
-            _isGenerator->setonly();
-            break;
-        default:
-            HError("Unknown input source type %d", _app->GetInputSourceType());
+    if( _mode == EDIT ) {
+        switch (_app->GetInputSourceType()) {
+            case NETWORK:
+                _isRemote->setonly();
+                break;
+            case AUDIO_DEVICE:
+                _isAudioDevice->setonly();
+                break;
+            case InputSourceType::RTLSDR:
+                _isRtlsdrDevice->setonly();
+                break;
+            case InputSourceType::PCM_FILE:
+                _isPcmFile->setonly();
+                break;
+            case InputSourceType::WAV_FILE:
+                _isWavFile->setonly();
+                break;
+            case InputSourceType::SILENCE:
+                _isSilence->setonly();
+                break;
+            case InputSourceType::SIGNAL_GENERATOR:
+                _isGenerator->setonly();
+                break;
+            default:
+                HError("Unknown input source type %d", _app->GetInputSourceType());
+        }
+    } else {
+        _isSilence->setonly();
     }
 
     // Hardware input devices
     std::map<int, std::string> devices = _app->GetAudioDevices(true, true, true, false);
     for( std::map<int, std::string>::iterator it = devices.begin(); it != devices.end(); it++ ) {
         _localAudioDevice->add((*it).second.c_str());
-        if( (*it).first == _app->GetInputDevice() ) {
-            _localAudioDevice->value((*it).first - 1);
+        if( _mode == EDIT ) {
+            if( (*it).first == _app->GetInputDevice() ) {
+                _localAudioDevice->value((*it).first - 1);
+            }
         }
     }
     devices = _app->GetRtlsdrDevices();
     for( std::map<int, std::string>::iterator it = devices.begin(); it != devices.end(); it++ ) {
         _localRtlsdrDevice->add((*it).second.c_str());
-        if( (*it).first == _app->GetInputDevice() ) {
-            _localRtlsdrDevice->value((*it).first - 1);
+        if( _mode == EDIT ) {
+            if( (*it).first == _app->GetInputDevice() ) {
+                _localRtlsdrDevice->value((*it).first - 1);
+            }
         }
     }
 
     // Various settings
-    _localPcmFilename->value(_app->GetPcmFile().c_str());
-    _localWavFilename->value(_app->GetWavFile().c_str());
-    _localGeneratorFrequency->value(std::to_string(_app->GetSignalGeneratorFrequency()).c_str());
-    _remoteHost->value(_app->GetRemoteServer().c_str());
-    _remoteDataPort->value(std::to_string(_app->GetRemoteDataPort()).c_str());
-    _remoteCommandPort->value(std::to_string(_app->GetRemoteCommandPort()).c_str());
+    if( _mode == EDIT ) {
+        _localPcmFilename->value(_app->GetPcmFile().c_str());
+        _localWavFilename->value(_app->GetWavFile().c_str());
+        _localGeneratorFrequency->value(std::to_string(_app->GetSignalGeneratorFrequency()).c_str());
+        _remoteHost->value(_app->GetRemoteServer().c_str());
+        _remoteDataPort->value(std::to_string(_app->GetRemoteDataPort()).c_str());
+        _remoteCommandPort->value(std::to_string(_app->GetRemoteCommandPort()).c_str());
+    }
 
     // Samplerates
     int i = 0;
     for( std::vector<int>::iterator it = _deviceRates.begin(); it != _deviceRates.end(); it++ ) {
         _deviceRate->add(std::to_string((*it)).c_str());
-        if( (*it) == _app->GetInputSampleRate()) {
-            _deviceRate->value(i);
+        if( _mode == EDIT ) {
+            if( (*it) == _app->GetInputSampleRate() ) {
+                _deviceRate->value(i);
+            }
+        } else {
+            if( (*it) == 48000 ) {
+                _deviceRate->value(i);
+            }
         }
         i++;
     }
     i = 0;
     for( std::vector<int>::iterator it = _outputRates.begin(); it != _outputRates.end(); it++ ) {
         _outputRate->add(std::to_string((*it)).c_str());
-        if( (*it) == _app->GetOutputSampleRate()) {
-            _outputRate->value(i);
+        if( _mode == EDIT ) {
+            if( (*it) == _app->GetOutputSampleRate() ) {
+                _outputRate->value(i);
+            }
+        } else {
+            if( (*it) == 48000 ) {
+                _outputRate->value(i);
+            }
         }
         i++;
     }
 
     // Original input type (if none is set, that's perfectly allright. We may not need it)
-    switch(_app->GetOriginalInputSourceType()) {
-        case AUDIO_DEVICE:
-            _isOriginalAudioDevice->setonly();
-            break;
-        case RTLSDR:
-            _isOriginalRtlsdrDevice->setonly();
-            break;
-        case PCM_FILE:
-            _isOriginalPcmFile->setonly();
-            break;
-        case WAV_FILE:
-            _isOriginalWavFile->setonly();
-            break;
-        case SILENCE:
-            _isOriginalSilence->setonly();
-            break;
-        case SIGNAL_GENERATOR:
-            _isOriginalGenerator->setonly();
-            break;
-        default:
-            break;
+    if( _mode == EDIT ) {
+        switch (_app->GetOriginalInputSourceType()) {
+            case AUDIO_DEVICE:
+                _isOriginalAudioDevice->setonly();
+                break;
+            case RTLSDR:
+                _isOriginalRtlsdrDevice->setonly();
+                break;
+            case PCM_FILE:
+                _isOriginalPcmFile->setonly();
+                break;
+            case WAV_FILE:
+                _isOriginalWavFile->setonly();
+                break;
+            case SILENCE:
+                _isOriginalSilence->setonly();
+                break;
+            case SIGNAL_GENERATOR:
+                _isOriginalGenerator->setonly();
+                break;
+            default:
+                break;
+        }
+    } else {
+        _isOriginalSilence->setonly();
     }
 
     // Input sample type
-    switch(_app->GetInputSourceDataType()) {
-        case REAL_INPUT_SOURCE_DATA_TYPE:
-            _inputSourceDatatypeReal->setonly();
-            break;
-        case IQ_INPUT_SOURCE_DATA_TYPE:
-            _inputSourceDatatypeIq->setonly();
-            break;
-        case I_INPUT_SOURCE_DATA_TYPE:
-            _inputSourceDatatypeI->setonly();
-            break;
-        case Q_INPUT_SOURCE_DATA_TYPE:
-            _inputSourceDatatypeQ->setonly();
-            break;
-        default:
-            break;
+    if( _mode == EDIT ) {
+        switch (_app->GetInputSourceDataType()) {
+            case REAL_INPUT_SOURCE_DATA_TYPE:
+                _inputSourceDatatypeReal->setonly();
+                break;
+            case IQ_INPUT_SOURCE_DATA_TYPE:
+                _inputSourceDatatypeIq->setonly();
+                break;
+            case I_INPUT_SOURCE_DATA_TYPE:
+                _inputSourceDatatypeI->setonly();
+                break;
+            case Q_INPUT_SOURCE_DATA_TYPE:
+                _inputSourceDatatypeQ->setonly();
+                break;
+            default:
+                break;
+        }
+    } else {
+        _inputSourceDatatypeReal->setonly();
     }
 
     // Update gui state
@@ -345,10 +377,6 @@ void InputDialog::UpdateState() {
         if( _originalInputTypeGroup->active() == 0 ) {
             _originalInputTypeGroup->set_active();
             _originalInputTypeGroup->redraw();
-        }
-        if( _inputSourceDataTypeGroup->active() == 0 ) {
-            _inputSourceDataTypeGroup->set_active();
-            _inputSourceDataTypeGroup->redraw();
         }
 
         _localAudioDevice->clear_active();
@@ -380,10 +408,8 @@ void InputDialog::UpdateState() {
     if( _isAudioDevice->value() == 1 ) {
         _remoteInputTypeGroup->clear_active();
         _originalInputTypeGroup->clear_active();
-        _inputSourceDataTypeGroup->clear_active();
         _remoteInputTypeGroup->redraw();
         _originalInputTypeGroup->redraw();
-        _inputSourceDataTypeGroup->redraw();
 
         _localAudioDevice->set_active();
         _localRtlsdrDevice->clear_active();
@@ -412,10 +438,8 @@ void InputDialog::UpdateState() {
     if( _isRtlsdrDevice->value() == 1 ) {
         _remoteInputTypeGroup->clear_active();
         _originalInputTypeGroup->clear_active();
-        _inputSourceDataTypeGroup->clear_active();
         _remoteInputTypeGroup->redraw();
         _originalInputTypeGroup->redraw();
-        _inputSourceDataTypeGroup->redraw();
 
         _localAudioDevice->clear_active();
         _localRtlsdrDevice->set_active();
@@ -447,10 +471,6 @@ void InputDialog::UpdateState() {
         if( _originalInputTypeGroup->active() == 0 ) {
             _originalInputTypeGroup->set_active();
             _originalInputTypeGroup->redraw();
-        }
-        if( _inputSourceDataTypeGroup->active() == 0 ) {
-            _inputSourceDataTypeGroup->set_active();
-            _inputSourceDataTypeGroup->redraw();
         }
 
         _localAudioDevice->clear_active();
@@ -484,10 +504,6 @@ void InputDialog::UpdateState() {
             _originalInputTypeGroup->set_active();
             _originalInputTypeGroup->redraw();
         }
-        if( _inputSourceDataTypeGroup->active() == 0 ) {
-            _inputSourceDataTypeGroup->set_active();
-            _inputSourceDataTypeGroup->redraw();
-        }
 
         _localAudioDevice->clear_active();
         _localRtlsdrDevice->clear_active();
@@ -516,10 +532,8 @@ void InputDialog::UpdateState() {
     if( _isSilence->value() == 1 ) {
         _remoteInputTypeGroup->clear_active();
         _originalInputTypeGroup->clear_active();
-        _inputSourceDataTypeGroup->clear_active();
         _remoteInputTypeGroup->redraw();
         _originalInputTypeGroup->redraw();
-        _inputSourceDataTypeGroup->redraw();
 
         _localAudioDevice->clear_active();
         _localRtlsdrDevice->clear_active();
@@ -544,10 +558,8 @@ void InputDialog::UpdateState() {
     if( _isGenerator->value() == 1 ) {
         _remoteInputTypeGroup->clear_active();
         _originalInputTypeGroup->clear_active();
-        _inputSourceDataTypeGroup->clear_active();
         _remoteInputTypeGroup->redraw();
         _originalInputTypeGroup->redraw();
-        _inputSourceDataTypeGroup->redraw();
 
         _localAudioDevice->clear_active();
         _localRtlsdrDevice->clear_active();
