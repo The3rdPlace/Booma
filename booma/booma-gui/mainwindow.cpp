43d95dcb7abb8148b7a609eb906b7654b964586c
Henrik Witt-Hansen <henrik@hardttoolkit.org>
2021-04-12 21:09:08 +0200

    WIP: Initial prototype and testing of waterfall drawing

--- booma/booma-gui/mainwindow.cpp
+++ booma/booma-gui/mainwindow.cpp
@@ -101,7 +101,7 @@ MainWindow::MainWindow(BoomaApplication* app):
     } );
     _rfSpectrumThread = new std::thread( [this]()  {
         while( _threadsRunning ) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
             UpdateRfSpectrumDisplay();
         }
     } );
--- booma/booma-gui/waterfall.cpp
+++ booma/booma-gui/waterfall.cpp
@@ -5,4 +5,12 @@ void Waterfall::draw() {
     fl_color(FL_BLACK);
     fl_draw("RF waterfall", 20, 60);
 
+    char pixmap[100];
+    for( int i = 0; i < 100; i += 4 ) {
+        fl_point(20 + i, 100);
+        fl_point(20 + i + 1, 100);
+    }
+Fl_Offscreen ofscr;
+
+    
 }
